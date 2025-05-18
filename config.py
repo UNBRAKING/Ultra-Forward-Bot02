908fd436b477af9f7a3e1c36d7d094c5f36861cc
UNBRAKING THINGS <code.pbail@gmail.com>
2025-05-18 21:27:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "26614080")
-    API_HASH = os.environ.get("API_HASH", "7d2c9a5628814e1430b30a1f0dc0165b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8017499587:AAHwx8VdtQsf83ujqKCF8xnByuT-8gLZrYk") 
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://xefiw96298:TVM8AI1hG5dA2kSc@cluster0.avacy9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "team_pbail")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", "5606990991").split()]
+    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
 
 
 class temp(object): 
